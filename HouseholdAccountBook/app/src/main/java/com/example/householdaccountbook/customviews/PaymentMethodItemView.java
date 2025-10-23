227a993baa82ea1b799d160b68963febf2771a40
Dankarton <danyuuhig@gmail.com>
2025-10-23 18:18:55 +0900

    支払い方法リストアップActivity作成

---
+++ HouseholdAccountBook/app/src/main/java/com/example/householdaccountbook/customviews/PaymentMethodItemView.java
@@ -0,0 +1,44 @@
+package com.example.householdaccountbook.customviews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.example.householdaccountbook.R;
+
+import myclasses.PaymentMethod;
+
+public class PaymentMethodItemView extends ConstraintLayout {
+    private PaymentMethod data;
+    private TextView nameText;
+    public PaymentMethodItemView(@NonNull Context context) {
+        super(context);
+        init(context);
+    }
+
+    public PaymentMethodItemView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public PaymentMethodItemView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+    private void init(Context context) {
+        LayoutInflater.from(context).inflate(R.layout.custom_view_payment_method_item, this, true);
+        this.nameText = findViewById(R.id.name_text);
+    }
+    public void setData(PaymentMethod data) {
+        this.data = data;
+        changeText();
+    }
+    private void changeText() {
+        this.nameText.setText(this.data.getName());
+    }
+}
--- HouseholdAccountBook/app/src/main/java/com/example/householdaccountbook/fragments/PaymentMethodListingFragment.java
+++ HouseholdAccountBook/app/src/main/java/com/example/householdaccountbook/fragments/PaymentMethodListingFragment.java
@@ -1,21 +1,30 @@
 package com.example.householdaccountbook.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.example.householdaccountbook.MyDbManager;
 import com.example.householdaccountbook.R;
+import com.example.householdaccountbook.customviews.PaymentMethodItemView;
+
+import java.util.ArrayList;
+
+import myclasses.PaymentMethod;
 
 /**
  * A simple {@link Fragment} subclass.
  * create an instance of this fragment.
  */
 public class PaymentMethodListingFragment extends Fragment {
-
+    private LinearLayout itemList;
     public PaymentMethodListingFragment() {
         // Required empty public constructor
     }
@@ -30,5 +39,30 @@ public class PaymentMethodListingFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_payment_method_listing, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        this.itemList = view.findViewById(R.id.payment_method_list);
+        ArrayList<PaymentMethod> methods = MyDbManager.getAllPaymentMethodData();
+        PaymentMethodItemView itemView = new PaymentMethodItemView(view.getContext());
+        itemView.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }
+        );
+    }
+    private PaymentMethodItemView createPaymentMethodItemView(Context context, PaymentMethod data) {
+        PaymentMethodItemView itemView = new PaymentMethodItemView(context);
+        itemView.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        
+                    }
+                }
+        )
     }
 }
