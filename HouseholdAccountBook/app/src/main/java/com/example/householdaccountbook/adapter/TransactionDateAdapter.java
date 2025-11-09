136ff64545601a856b6aecb5cf65526382384e91
Dankarton <124846114+Dankarton@users.noreply.github.com>
2025-11-09 16:51:45 +0900

    Revert "間違えた"
    This reverts commit e4b2edbf3e8e9014ecde339cb77ade065c949815.

--- HouseholdAccountBook/app/src/main/java/com/example/householdaccountbook/adapter/TransactionDateAdapter.java
+++ HouseholdAccountBook/app/src/main/java/com/example/householdaccountbook/adapter/TransactionDateAdapter.java
@@ -2,7 +2,6 @@ package com.example.householdaccountbook.adapter;
 
 import android.content.Context;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -17,41 +16,24 @@ import myclasses.BOP;
 import myclasses.DailyBop;
 
 public class TransactionDateAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    private enum ViewType {
-        DAILY_BRIEF,
-        INCOME,
-        PURCHASE,
-        EXPENSES
-    }
     public interface OnListItemActionListener {
         void OnActionButtonClicked(BOP data);
     }
     private OnListItemActionListener listener;
     private final Context context;
-    private final List<Object> itemDataList;
+    private final List<DailyBop> dailyBopList;
     private final RecyclerView.RecycledViewPool viewPool = new RecyclerView.RecycledViewPool();
 
-    public TransactionDateAdapter(Context context, List<Object> dataList) {
+    public TransactionDateAdapter(Context context, List<DailyBop> dataList) {
         this.context = context;
-        this.itemDataList = dataList;
+        this.dailyBopList = dataList;
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        if (viewType == ViewType.DAILY_BRIEF.ordinal()) {
-
-        }
-        else if (viewType == ViewType.INCOME.ordinal()) {
-
-        }
-        else if (viewType == ViewType.PURCHASE.ordinal()) {
-
-        }
-        else {
-
-        }
+        DailyRecordCustomView view = new DailyRecordCustomView(parent.getContext());
+        return new TransactionViewHolder(view);
     }
 
     @Override
@@ -69,5 +51,27 @@ public class TransactionDateAdapter extends RecyclerView.Adapter<RecyclerView.Vi
         return this.dailyBopList.size();
     }
     //
+    static class TransactionViewHolder extends RecyclerView.ViewHolder {
+        private final DailyRecordCustomView dailyView;
+        private DailyRecordAdapter subAdapter;
 
+        public TransactionViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.dailyView = (DailyRecordCustomView) itemView;
+        }
+
+        public void bind(DailyBop dailyData, OnListItemActionListener listener, RecyclerView.RecycledViewPool viewPool) {
+            if (this.subAdapter == null) {
+                subAdapter = new DailyRecordAdapter(itemView.getContext(), dailyData.getAdapterDataList());
+                subAdapter.setListener(new DailyRecordAdapter.OnListItemActionListener() {
+                    @Override
+                    public void onMoreActionButtonClicked(BOP data) {
+                        listener.OnActionButtonClicked(data);
+                    }
+                });
+                this.dailyView.setRecycledViewPool(viewPool);
+            }
+            this.dailyView.bind(dailyData.getYear(), dailyData.getMonth(), dailyData.getDate(), subAdapter);
+        }
+    }
 }
